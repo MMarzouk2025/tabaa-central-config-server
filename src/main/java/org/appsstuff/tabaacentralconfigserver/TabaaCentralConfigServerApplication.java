601775084607f9ef28f8c1e0c6a8f1dd0e4a667e
MahmoudMarzouk2020 <MahmoudMarzouk2016@gmail.com>
2021-11-12 19:31:45 +0200

    Enabling Configuration Server

--- src/main/java/org/appsstuff/tabaacentralconfigserver/TabaaCentralConfigServerApplication.java
+++ src/main/java/org/appsstuff/tabaacentralconfigserver/TabaaCentralConfigServerApplication.java
@@ -2,8 +2,10 @@ package org.appsstuff.tabaacentralconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class TabaaCentralConfigServerApplication {
 
     public static void main(String[] args) {
