278e962055d25fbe5e021c73968b96fe4ce6098b
MahmoudMarzouk2020 <MahmoudMarzouk2016@gmail.com>
2021-11-12 17:33:24 +0200

    Initial commit

---
+++ src/test/java/org/appsstuff/tabaacentralconfigserver/TabaaCentralConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.appsstuff.tabaacentralconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabaaCentralConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
